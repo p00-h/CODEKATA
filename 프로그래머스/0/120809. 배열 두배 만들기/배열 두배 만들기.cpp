a11edef8a6403aa19ce2f2eb80d149589b4bc9e2
p00-h <149079533+p00-h@users.noreply.github.com>
2025-08-14 10:53:02 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.25 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    for(int number : numbers){
+        answer.push_back(number * 2);
+    }
+    return answer;
+}
