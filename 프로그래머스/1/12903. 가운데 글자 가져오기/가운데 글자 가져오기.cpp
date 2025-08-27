b9d8d61f5c3c630e0a1445aa10bc1cb54f9c62ff
p00-h <149079533+p00-h@users.noreply.github.com>
2025-08-27 09:09:58 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 4.04 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int len = s.size();
+        
+    if (len % 2 == 0){
+        answer = s.substr((len / 2)-1,2);    
+    }else if (len % 2 == 1){
+        answer = s[len / 2];
+    }
+    return answer;
+}
