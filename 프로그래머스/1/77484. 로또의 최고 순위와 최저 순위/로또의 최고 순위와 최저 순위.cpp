738e0b1ba6a63c13ac6352660c66672a88f171bf
p00-h <149079533+p00-h@users.noreply.github.com>
2025-09-23 10:06:47 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
@@ -4,45 +4,36 @@
 
 using namespace std;
 
+int result(int a){
+    switch(a){
+        case 2: return 5; break;
+        case 3: return 4; break;
+        case 4: return 3; break;
+        case 5: return 2; break;
+        case 6: return 1; break;
+        default:return 6; break;
+    }
+}
+
 vector<int> solution(vector<int> lottos, vector<int> win_nums) {
     vector<int> answer;
     int count = 0;
     int zeroCount = 0;
     
     for(int i = 0; i < lottos.size(); i++){
-        
         if(lottos[i] == 0){
             zeroCount++;
             for(int j= 0; j < win_nums.size(); j ++){
-                auto x = find(lottos.begin(),lottos.end(),win_nums[j]);
-                if(x == lottos.end()){
+                if((find(lottos.begin(),lottos.end(),win_nums[j])) == lottos.end()){
                     lottos[i] = win_nums[j];
                 }
             }
         } 
-        
-        if((find(win_nums.begin(),win_nums.end(),lottos[i])) != win_nums.end()){
-            count++;
-        }        
+        if((find(win_nums.begin(),win_nums.end(),lottos[i])) != win_nums.end()) count++;
     }
     
-    switch(count){
-        case 2: answer.push_back(5); break;
-        case 3: answer.push_back(4); break;
-        case 4: answer.push_back(3); break;
-        case 5: answer.push_back(2); break;
-        case 6: answer.push_back(1); break;
-        default:answer.push_back(6); break;
-    }
-        
-    switch(count-zeroCount){
-        case 2: answer.push_back(5); break;
-        case 3: answer.push_back(4); break;
-        case 4: answer.push_back(3); break;
-        case 5: answer.push_back(2); break;
-        case 6: answer.push_back(1); break;
-        default:answer.push_back(6); break;
-    }
+    answer.push_back(result(count));
+    answer.push_back(result(count - zeroCount));
     
     return answer;
 }
